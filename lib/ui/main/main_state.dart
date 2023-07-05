5e529c504a558e75320a7ba8882e33048902a711
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-06 01:23:40 +0900

    fix: freezed 사용하여 main state 수정

--- lib/ui/main/main_state.dart
+++ lib/ui/main/main_state.dart
@@ -1,21 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_search_4day/data/model/photo.dart';
 
-class MainState {
-  final List<Photo> photos;
-  final bool isLoading;
+part 'main_state.freezed.dart';
 
-  MainState({
-    required this.photos,
-    required this.isLoading,
-  });
+part 'main_state.g.dart';
 
-  MainState copyWith({
-    List<Photo>? photos,
-    bool? isLoading,
-  }) {
-    return MainState(
-      photos: photos ?? this.photos,
-      isLoading: isLoading ?? this.isLoading,
-    );
-  }
+@freezed
+class MainState with _$MainState {
+  const factory MainState({
+    @Default([]) List<Photo> photos,
+    @Default(false) bool isLoading,
+  }) = _MainState;
+
+  factory MainState.fromJson(Map<String, Object?> json) =>
+      _$MainStateFromJson(json);
 }
