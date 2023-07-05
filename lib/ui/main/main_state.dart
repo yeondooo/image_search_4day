10e111aedc7642179e0f8c7876c796b82889446d
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-06 01:12:33 +0900

    fix: 불변객체로 수정하기위해 copyWith메서드 생성

--- lib/ui/main/main_state.dart
+++ lib/ui/main/main_state.dart
@@ -1,6 +1,21 @@
 import 'package:image_search_4day/data/model/photo.dart';
 
 class MainState {
-  List<Photo> photos = [];
-  bool isLoading = false;
+  final List<Photo> photos;
+  final bool isLoading;
+
+  MainState({
+    required this.photos,
+    required this.isLoading,
+  });
+
+  MainState copyWith({
+    List<Photo>? photos,
+    bool? isLoading,
+  }) {
+    return MainState(
+      photos: photos ?? this.photos,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
 }
