5d627468c5c05d8198d1bbbf105cc462837df000
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-06 01:14:03 +0900

    fix: 불변객체로 수정된 main state에 따라 main view model 수정

--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_4day/data/model/photo.dart';
 import 'package:image_search_4day/repository/photo_repository.dart';
+import 'package:image_search_4day/ui/main/main_state.dart';
 
 class MainViewModel with ChangeNotifier {
   final PhotoRepository _repository;
 
   MainViewModel(this._repository);
 
-  List<Photo> _photos = [];
+  MainState _state = MainState(photos: [], isLoading: false);
 
-  List<Photo> get photos => _photos;
-
-  bool _isLoading = false;
-
-  bool get isLoading => _isLoading;
+  MainState get state => _state;
 
   void fetchImages(String query) async {
-    _isLoading = true;
+    _state = state.copyWith(isLoading: true);
     notifyListeners();
 
-    _photos = await _repository.getPhotos(query);
+    final photos = await _repository.getPhotos(query);
 
-    _isLoading = false;
+    _state = state.copyWith(
+      photos: photos,
+      isLoading: false,
+    );
     notifyListeners();
   }
 }
