32891ed7bf6768ac04da7bcebdd540d197748486
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-06 00:54:25 +0900

    feat: main screen 구현

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_4day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,13 +11,64 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('이미지 검색 앱'),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              hintText: '이미지를 입력하세요',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  viewModel.fetchImages(controller.text);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+              ),
+              itemCount: viewModel.photos.length,
+              itemBuilder: (context, index) {
+                final photo = viewModel.photos[index];
+                return Hero(
+                  tag: photo.id,
+                  child: GestureDetector(
+                    onTap: () {
+                      context.push('/detail', extra: photo);
+                    },
+                    child: Image.network(
+                      photo.webformatURL,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
