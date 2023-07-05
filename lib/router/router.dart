f91e4372e56a678b5932c7d06618f9a49aac437e
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-06 00:53:18 +0900

    fix: router에 ChangeNotifierProvider 추가

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_search_4day/data/model/photo.dart';
+import 'package:image_search_4day/repository/pixabay_photo_repository_impl.dart';
 import 'package:image_search_4day/ui/detail/detail_screen.dart';
 import 'package:image_search_4day/ui/main/main_screen.dart';
+import 'package:image_search_4day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const MainScreen();
+        return ChangeNotifierProvider(
+          create: (_) =>
+              MainViewModel(
+                PixabayPhotoRepositoryImpl(),
+              ),
+          child: const MainScreen(),
+        );
       },
       routes: <RouteBase>[
         GoRoute(
           path: 'detail',
           builder: (BuildContext context, GoRouterState state) {
-            return const DetailScreen();
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
           },
         ),
       ],
